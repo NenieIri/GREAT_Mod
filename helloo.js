e7a876b3f9bee2a93db8f4612c520cb313e25b2e
NenieIri <46263858+NenieIri@users.noreply.github.com>
2019-01-03 20:29:48 +0400

    Update helloo.js

--- helloo.js
+++ helloo.js
@@ -10,28 +10,38 @@ function hello(G){
   var dnaWallet = G.querySelector('#dnaWallet').firstChild.innerHTML;
   var coinWallet = G.querySelector('#coinWallet').firstChild.innerHTML;
   var user_name = G.querySelector('#mainui-user .user-container .user-name').innerHTML;
+  var progress_bar_text = G.querySelector('#progressBar .progress-bar-text').innerHTML;
+  var progress_bar_level = G.querySelector('#progressBar .progress-bar-star span').innerHTML;
   
   var mainui_user = '<div class="user-container">';
-  mainui_user += '<img class="user-picture" src="'+user_IMG+'">';
-  mainui_user += '<div id="dnaWallet">';
-  mainui_user +=    '<span class="labelF2">'+dnaWallet+'</span>';
-  mainui_user +=    '<span class="sprite-main dna-small-icon"></span>';
-  mainui_user +=    '<div class="plusF2"> <span>+</span> </div>';
-  mainui_user += '</div>';
-  mainui_user += '<div id="coinWallet">';
-  mainui_user +=    '<span class="labelF2">'+coinWallet+'</span>';
-  mainui_user +=    '<span class="sprite-main currency-icon"></span>';
-  mainui_user +=    '<div class="plusF2"> <span>+</span> </div>';
-  mainui_user += '</div>';
-  mainui_user += '<div class="user-nameF1">'+user_name+'</div>';
-  
-  mainui_user += '<div id="progressBarF1"> <div class="progress-bar-container">';
-  mainui_user += '<div class="progress-barF3" style="width: 0%;"></div><span class="progress-bar-textF3">0/50 XP</span> </div>';
-  mainui_user += '<div class="progress-bar-border"></div><div class="progress-bar-starF3 sprite leaguestar-fly"> <span>1</span> </div>';
-  mainui_user += '</div><div id="xpButton"> <span class="boost-image sprite-main xpboost-3x-shop"></span>';
-  mainui_user += '<div data-f5="" class="circle green"> <span>+</span> </div></div>';
-  mainui_user += '<div id="massButton"> <span class="boost-image sprite-main massboost-3x-shop"></span>';
-  mainui_user += '<div data-f5="" class="circle green"> <span>+</span> </div></div></div>';
+      mainui_user += '<img class="user-picture" src="'+user_IMG+'">';
+      mainui_user += '<div id="dnaWallet">';
+      mainui_user +=    '<span class="labelF2">'+dnaWallet+'</span>';
+      mainui_user +=    '<span class="sprite-main dna-small-icon"></span>';
+      mainui_user +=    '<div class="plusF2"> <span>+</span> </div>';
+      mainui_user += '</div>';
+      mainui_user += '<div id="coinWallet">';
+      mainui_user +=    '<span class="labelF2">'+coinWallet+'</span>';
+      mainui_user +=    '<span class="sprite-main currency-icon"></span>';
+      mainui_user +=    '<div class="plusF2"> <span>+</span> </div>';
+      mainui_user += '</div>';
+      mainui_user += '<div class="user-nameF1">'+user_name+'</div>';
+
+      mainui_user += '<div id="progressBarF1">';
+      mainui_user +=    '<div class="progress-bar-container">';
+      mainui_user +=        '<div class="progress-barF3" style="width: 0%;"></div>';
+      mainui_user +=        '<span class="progress-bar-textF3">'+progress_bar_text+'</span>';
+      mainui_user +=     '</div>';
+      mainui_user +=     '<div class="progress-bar-border"></div>';
+      mainui_user +=     '<div class="progress-bar-starF3 sprite leaguestar-fly">';
+      mainui_user +=        '<span>'+progress_bar_level+'</span>';
+      mainui_user +=     '</div>';
+      mainui_user += '</div>';
+      
+      mainui_user += '<div id="xpButton"> <span class="boost-image sprite-main xpboost-3x-shop"></span>';
+      mainui_user += '<div data-f5="" class="circle green"> <span>+</span> </div></div>';
+      mainui_user += '<div id="massButton"> <span class="boost-image sprite-main massboost-3x-shop"></span>';
+      mainui_user += '<div data-f5="" class="circle green"> <span>+</span> </div></div></div>';
   
   G.getElementById('mainui-user').innerHTML=mainui_user;
 }
