3c575e5778103b7cb2c9103ed2b1a0e6731b147c
NenieIri <46263858+NenieIri@users.noreply.github.com>
2019-01-03 18:15:25 +0400

    Update helloo.js

--- helloo.js
+++ helloo.js
@@ -1,2 +1,4 @@
-function hello(G){console.log(G.getElementById('mainui-user').innerHTML);}
-document.write("asdf");
+function hello(G){
+  console.log(G.getElementById('mainui-user').innerHTML);
+  document.write("asdf");
+}
