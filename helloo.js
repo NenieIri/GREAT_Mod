2facd7f7ada6c45952a66a87e80c26c1e0e6f9dc
NenieIri <46263858+NenieIri@users.noreply.github.com>
2019-01-09 23:39:43 +0400

    Update helloo.js

--- helloo.js
+++ helloo.js
@@ -25,8 +25,8 @@ function hello(G){
   var massButtonClass = G.querySelector('#massButton div').getAttribute('class');
   var massButtonCount = G.querySelector('#massButton div span').innerHTML;
   
-  xpButtonTimer = xpButtonTimer[1] ? JSON.stringify(xpButtonTimer[1]) : '';
-  massButtonTimer = massButtonTimer[1] ? JSON.stringify(massButtonTimer[1]).innerHTML : '';
+  xpButtonTimer = xpButtonTimer[1] ? xpButtonTimer[1] : '';
+  massButtonTimer = massButtonTimer[1] ? massButtonTimer[1] : '';
   
   var mainui_user = '<div class="user-container">';
       
